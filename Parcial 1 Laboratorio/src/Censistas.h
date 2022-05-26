ccd563adc32a8165646ddeb5d202e6cd6e54bfa7
franchureyes <franchureyes@gmail.com>
2022-05-26 19:58:01 -0300

    Finalizado

---
+++ Parcial 1 Laboratorio/src/Censistas.h
@@ -0,0 +1,25 @@
+/*
+ * Censistas.h
+ *
+ *  Created on: 21 may. 2022
+ *      Author: Fran
+ */
+
+#ifndef CENSISTAS_H_
+#define CENSISTAS_H_
+typedef struct
+{
+	int legajoCensista;
+	char nombre[25];
+	int edad;
+	char telefono[10];
+	int isEmpty;
+}Censista;
+
+int listarCensistas(Censista* censistas, int tamCen);
+int ordenarCensista(Censista* censistas, int tamCen);
+int validarCensista(Censista* censistas, int tamCen, int id);
+int buscarCensista(Censista* censistas, int tamCen, int id, int* pIndex);
+
+
+#endif /* CENSISTAS_H_ */
---
+++ Parcial 1 Laboratorio/src/inputs.h
@@ -0,0 +1,21 @@
+/*
+ * inputs.h
+ *
+ *  Created on: 13 may. 2022
+ *      Author: Fran
+ */
+
+#ifndef INPUTS_H_
+#define INPUTS_H_
+
+int myGets(char *cadena, int longitud);
+int getInt(int *pResultado);
+int esNumerica(char *cadena);
+int utn_getInt(int *pResultado, char *mensaje, char *mensajeError, int minimo, int maximo);
+int getFloat(float *pResultado);
+int utn_getFloat(float *pResultado, char *mensaje, char *mensajeError, float minimo, float maximo);
+int utn_getName(char* cadena, char* mensaje, char* mensajeError, int len);
+void utn_corregirMayus(char* cadena);
+
+
+#endif /* INPUTS_H_ */
